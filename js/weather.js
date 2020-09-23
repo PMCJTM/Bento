94d8879c65c3757e30c9a5d081d4c480396b9da1
PMCJTM <50344976+PMCJTM@users.noreply.github.com>
2020-09-23 12:10:54 -0400

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -5,15 +5,15 @@ const descElement = document.querySelector('.temperature-description p');
 // App data
 const weather = {};
 weather.temperature = {
-    unit: 'celsius',
+    unit: 'fahrenheit',
 };
 
 // Change to 'F' for Fahrenheit
-var tempUnit = 'C';
+var tempUnit = 'F';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '94eb249c20d2baa826087034a2f00853';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 40.678177;
+    let longitude = -73.944160;
 
     getWeather(latitude, longitude);
 }
